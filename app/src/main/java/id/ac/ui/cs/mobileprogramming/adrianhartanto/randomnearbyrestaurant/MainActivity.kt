98abc46c36603d831140996a5f1972378804486b
Adrian Hartanto <adrian.hartanto51@gmail.com>
2019-10-31 23:45:37 +0700

    Remove Butterknife view binding dependecies

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/adrianhartanto/randomnearbyrestaurant/MainActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/adrianhartanto/randomnearbyrestaurant/MainActivity.kt
@@ -6,23 +6,14 @@ import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import butterknife.BindView
-import butterknife.ButterKnife
-import butterknife.OnClick
 import com.google.gson.JsonElement
 import id.ac.ui.cs.mobileprogramming.adrianhartanto.randomnearbyrestaurant.product.AppApplication
-import id.ac.ui.cs.mobileprogramming.adrianhartanto.randomnearbyrestaurant.ui.main.MainFragment
 import id.ac.ui.cs.mobileprogramming.adrianhartanto.randomnearbyrestaurant.ui.main.MainViewModel
 import id.ac.ui.cs.mobileprogramming.adrianhartanto.randomnearbyrestaurant.utils.ApiResponse
 import id.ac.ui.cs.mobileprogramming.adrianhartanto.randomnearbyrestaurant.utils.ApiResponseStatus
 import id.ac.ui.cs.mobileprogramming.adrianhartanto.randomnearbyrestaurant.utils.ViewModelFactory
-import id.ac.ui.cs.mobileprogramming.adrianhartanto.randomnearbyrestaurant.utils.ViewModelFactory_Factory
 import javax.inject.Inject
-import androidx.core.app.ComponentActivity
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-
+import kotlinx.android.synthetic.main.main_activity.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -36,19 +27,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
-        ButterKnife.bind(this)
         (getApplication() as AppApplication).appComponent.doInjection(this)
 
         mainViewModel = ViewModelProviders.of(this, viewModelFactory).get(MainViewModel::class.java)
-        mainViewModel.hitGetCuisinesApi(-6.595038, 106.816635)
 
         mainViewModel.getCuisinesResponse().observe(this,
             Observer<ApiResponse> { apiResponse -> consumeResponse(apiResponse) })
-    }
 
-    @OnClick(R.id.button_get_cuisines)
-    fun onClickGetCuisines() {
-        mainViewModel.hitGetCuisinesApi(-6.595038, 106.816635)
+        button_get_cuisines.setOnClickListener { view ->
+            mainViewModel.hitGetCuisinesApi(-6.595038, 106.816635)
+        }
     }
 
     private fun consumeResponse(apiResponse: ApiResponse) {
@@ -62,7 +50,7 @@ class MainActivity : AppCompatActivity() {
             ApiResponseStatus.ERROR -> {
                 Toast.makeText(
                     this@MainActivity,
-                    resources.getString(R.string.errorString),
+                    resources.getString(R.string.error_string),
                     Toast.LENGTH_SHORT
                 ).show()
             }
@@ -78,7 +66,7 @@ class MainActivity : AppCompatActivity() {
         } else {
             Toast.makeText(
                 this@MainActivity,
-                resources.getString(R.string.errorString),
+                resources.getString(R.string.error_string),
                 Toast.LENGTH_SHORT
             ).show()
         }
